37aeaa1f6bdb14fd79b7875884b2b7382d7b2299
kunchamupendra <73011743+kunchamupendra@users.noreply.github.com>
2021-07-18 12:47:39 +0530

    Add files via upload

---
+++ Task_2/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import explore from './connect';
+
+ReactDOM.render(
+  
+    <App />
+  ,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <explore /> , 
+  document.getElementById('root1')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
